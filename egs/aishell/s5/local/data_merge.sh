821ab440fc09b498a1fe7372ddec89fba2787441
caiyueliang <393900414@qq.com>
2019-12-31 16:49:59 +0800

    update

--- egs/aishell/s5/local/data_merge.sh
+++ egs/aishell/s5/local/data_merge.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
+# 格式转换: C4_715 --> BAC009C0004W0715 = BAC009 C0004 W0715
+function name_standard {
+    prefix=BAC009
+    # echo "[name_standard] old name: "${1}
+    spk_id=`echo ${1} | cut -d '_' -f -1`
+    char_id=`echo ${spk_id:0:1}`
+    spk_id=`echo ${spk_id:1} | awk '{printf("%04d\n",$0)}'`
+    spk_id=${char_id}${spk_id}
+    # echo "[name_standard] spk_id: "${spk_id}
+    wav_id=`echo ${1} | cut -d '_' -f 2-`
+    wav_id=`echo ${wav_id} | awk '{printf("W%04d\n",$0)}'`
+    echo "[name_standard] wav_id: "${wav_id}
+    # echo "[name_standard] new name: "${prefix}${spk_id}${wav_id}
+}
+
+echo "[DATA_MERGE] 0 =================================="
 src_dir=/home/rd/caiyueliang/data/THCHS-30
 tar_dir=/home/rd/caiyueliang/data/AISHELL
+# AISHELL 音频ID格式为: BAC009S0724W0345 = BAC009 S0724 W0345
+# THCHS-30音频ID格式为: C4_715
+# 两者要保持一致：C4_715 --> BAC009C0004W0715 = BAC009 C0004 W0715
 prefix=BAC009
 
 echo "[DATA_MERGE] src_dir: "${src_dir}
