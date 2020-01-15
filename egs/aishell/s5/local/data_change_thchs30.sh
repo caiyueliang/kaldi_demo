8ba1e577ab2dcad9fbcf49cd174a40e57b1059cf
caiyueliang <393900414@qq.com>
2020-01-15 10:28:55 +0800

    update

--- egs/aishell/s5/local/data_change_thchs30.sh
+++ egs/aishell/s5/local/data_change_thchs30.sh
@@ -28,31 +28,35 @@ function get_spk_name_standard {
     return ${spk_id}
 }
 
-echo "[DATA_MERGE] 0 =================================="
+echo "[DATA_CHANGE] 0 =================================="
 src_dir=/home/rd/caiyueliang/data/THCHS-30
-tar_dir=/home/rd/caiyueliang/data/AISHELL
-echo "[DATA_MERGE] src_dir: "${src_dir}
-echo "[DATA_MERGE] tar_dir: "${tar_dir}
+out_dir=/home/rd/caiyueliang/data/THCHS-30_change
+echo "[DATA_CHANGE] src_dir: "${src_dir}
+echo "[DATA_CHANGE] out_dir: "${out_dir}
+
+if [ -d ${out_dir} ]; then
+    echo "[DATA_CHANGE] remove dir: "${out_dir}
+    rm -rf ${out_dir}
+fi
+mkdir ${out_dir}
 
 echo "[DATA_MERGE] 1 =================================="
 # 生成 THCHS-30 的 音频ID和对应的文本标签 的数据
 src_trans_dir=${src_dir}/data_thchs30/data
-tar_trans_dir=${tar_dir}/data_aishell/transcript_merge
-src_trans_file=${tar_dir}/data_aishell/transcript/aishell_transcript_v0.8.txt
-temp_trans_file=${tar_trans_dir}/thchs30_transcript_v1.0.txt
-tar_trans_file=${tar_trans_dir}/aishell_transcript_v0.8.txt
+out_trans_dir=${out_dir}/data_aishell/transcript
+temp_trans_file=${out_trans_dir}/thchs30_transcript_v1.0.txt
+out_trans_file=${out_trans_dir}/aishell_transcript_v0.8.txt
 echo "[DATA_MERGE]   src_trans_dir: "${src_trans_dir}
-echo "[DATA_MERGE]   tar_trans_dir: "${tar_trans_dir}
-echo "[DATA_MERGE]  src_trans_file: "${src_trans_file}
+echo "[DATA_MERGE]   out_trans_dir: "${out_trans_dir}
 echo "[DATA_MERGE] temp_trans_file: "${temp_trans_file}
-echo "[DATA_MERGE]  tar_trans_file: "${tar_trans_file}
+echo "[DATA_MERGE]  out_trans_file: "${out_trans_file}
 
-rm -rf ${tar_trans_dir}
-mkdir ${tar_trans_dir}
+mkdir ${out_dir}/data_aishell
+mkdir ${out_trans_dir}
 
 # find "/home/rd/caiyueliang/data/THCHS-30/data_thchs30/data" -name "*.wav.trn" | sort -u
-find ${src_trans_dir} -name "*.wav.trn" | sort -u > ${tar_trans_dir}/src_wav.txt
-for file in `cat ${tar_trans_dir}/src_wav.txt`; do
+find ${src_trans_dir} -name "*.wav.trn" | sort -u > ${out_trans_dir}/src_wav.txt
+for file in `cat ${out_trans_dir}/src_wav.txt`; do
     # file_id=`echo /home/rd/caiyueliang/data/THCHS-30/data_thchs30/data/D8_999.wav.trn | cut -d '/' -f 9- | cut -d '.' -f -1`
     # file_id=`echo -n ${file} | cut -d '/' -f 9- | cut -d '.' -f -1`
     file_id=`echo -n ${file##*/} | cut -d '.' -f -1`
@@ -76,22 +80,22 @@ for file in `cat ${tar_trans_dir}/src_wav.txt`; do
     cat ${file} | sed -n '1p' >> ${temp_trans_file}
 done
 
-cat ${src_trans_file} ${temp_trans_file} | sort -u > ${tar_trans_file} || exit 1;
+cat ${temp_trans_file} | sort -u > ${out_trans_file} || exit 1;
+rm -rf ${out_trans_dir}/src_wav.txt ${temp_trans_file}
 
 echo "[DATA_MERGE] 2 =================================="
 # 生成 lexicon.txt
 # /home/rd/caiyueliang/data/THCHS-30/data_thchs30/lm_word/lexicon.txt
 # /home/rd/caiyueliang/data/AISHELL/resource_aishell/lexicon.txt
-out_resource_dir=${tar_dir}/resource_aishell_merge
+out_resource_dir=${out_dir}/resource_aishell
 out_lexicon=${out_resource_dir}/lexicon.txt
 src_lexicon=${src_dir}/data_thchs30/lm_word/lexicon.txt
-tar_lexicon=${tar_dir}/resource_aishell/lexicon.txt
+tar_lexicon=${out_dir}/resource_aishell/lexicon.txt
 echo "[DATA_MERGE] out_resource_dir: "${out_resource_dir}
 echo "[DATA_MERGE]      out_lexicon: "${out_lexicon}
 echo "[DATA_MERGE]      src_lexicon: "${src_lexicon}
 echo "[DATA_MERGE]      tar_lexicon: "${tar_lexicon}
 
-rm -rf ${out_resource_dir}
 mkdir ${out_resource_dir}
 
 # 对字典去重并排序
@@ -104,9 +108,11 @@ echo "[DATA_MERGE] 3 =================================="
 # /home/rd/caiyueliang/data/THCHS-30/data_thchs30 含 dev/ test/ train/
 # /home/rd/caiyueliang/data/AISHELL/data_aishell/wav 含 dev/ test/ train/
 src_wav_dir=${src_dir}/data_thchs30
-tar_wav_dir=${tar_dir}/data_aishell/wav
+out_wav_dir=${out_dir}/data_aishell/wav
 echo "[DATA_MERGE] src_wav_dir: "${src_wav_dir}
-echo "[DATA_MERGE] tar_wav_dir: "${tar_wav_dir}
+echo "[DATA_MERGE] out_wav_dir: "${out_wav_dir}
+
+mkdir ${out_wav_dir}
 
 for dir in dev test train; do
     # find /home/rd/caiyueliang/data/THCHS-30/data_thchs30/dev -name "*.wav"
@@ -134,11 +140,15 @@ for dir in dev test train; do
         #dir_name=$(get_spk_name_standard ${file_id})
         dir_name=${spk_id}
 
-        if [ ! -d ${tar_wav_dir}/${dir}/${dir_name} ]; then
-            echo "[DATA_MERGE] mkdir: "${tar_wav_dir}/${dir}/${dir_name}
-            mkdir ${tar_wav_dir}/${dir}/${dir_name}
+        if [ ! -d ${out_wav_dir}/${dir} ]; then
+            echo "[DATA_MERGE] mkdir: "${out_wav_dir}/${dir}
+            mkdir ${out_wav_dir}/${dir}
         fi
-        cp -r ${file} ${tar_wav_dir}/${dir}/${dir_name}/${new_file_id}".wav" || exit 1;
+        if [ ! -d ${out_wav_dir}/${dir}/${dir_name} ]; then
+            echo "[DATA_MERGE] mkdir: "${out_wav_dir}/${dir}/${dir_name}
+            mkdir ${out_wav_dir}/${dir}/${dir_name}
+        fi
+        cp -r ${file} ${out_wav_dir}/${dir}/${dir_name}/${new_file_id}".wav" || exit 1;
     done
     rm -r ${src_wav_dir}/${dir}_wav.txt
 done
