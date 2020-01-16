44ecbd0c21de89954d5a780be883aa8a99579fce
caiyueliang <393900414@qq.com>
2020-01-16 18:10:40 +0800

    update

---
+++ egs/thchs30/online_demo/wav_change.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+in_dir="./online-data/audio/qddata"
+out_dir="./online-data/audio/qddata_16khz"
+
+if [ -d ${out_dir} ]; then
+    rm -rf ${out_dir}
+fi
+mkdir ${out_dir}
+
+for x in ${in_dir}/*.wav
+do
+    echo ${in_dir}/${x}
+    name=${x##*/}
+    sox ${in_dir}/${name} -r 16000 ${out_dir}/${name}
+done
